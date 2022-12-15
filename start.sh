e5826ed97215e83d0a78dca8c1751a2b9ba161ec
Dingrong_Wang <dw7445@rit.edu>
2022-12-15 14:46:09 -0500

    RC

--- start.sh
+++ start.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 echo "Bash version ${BASH_VERSION}..."
 
+#SBATCH --mail-user=986739772@qq.com
+#SBATCH --mail-type=ALL
+#SBATCH -A vision -p tier3 -n 2
+#SBATCH -c 1
+#SBATCH --mem=4g
+#SBATCH --gres=gpu:a100:1
+
+conda activate LTH
+
 script="$0"
 arch="$1"
 dataset="$2"
