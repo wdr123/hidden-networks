7cc4d3438e3857a2d71af5a5954259bc51ea2425
Dingrong_Wang <dw7445@rit.edu>
2022-12-15 14:39:23 -0500

    RC

--- submit_jobs.sh
+++ submit_jobs.sh
@@ -9,7 +9,7 @@ mkdir -p $dir
 array=(0 0.1 0.3 0.5 0.7 0.9)
 for i in "${array[@]}"
 do
-  export $i
+  export i
   export arch="$1" dataset="$2"
   job_name=$base_job_name-$((arch))-$((dataset))-"${i}"
   out_file=$dir/$job_name.out
